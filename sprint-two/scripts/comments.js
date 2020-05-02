2b791de57de256f2c69dcdfe980cc962db582821
Steven Martins <Steven@MacBook-Pro.local>
2020-05-02 02:08:19 -0700

    friday night commit

--- sprint-two/scripts/comments.js
+++ sprint-two/scripts/comments.js
@@ -10,19 +10,19 @@ commentsForm.addEventListener("submit", displayComment);
 const comments = [
   {
     name: "Micheal Lyons",
-    date: new Date("12 / 18 / 2018"),
+    date: "12 / 18 / 2018",
     content:
       "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
   },
   {
     name: "Gary Wong",
-    date: new Date("12 / 12 / 2018"),
+    date: "12 / 12 / 2018",
     content:
       "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
   },
   {
     name: "Theodore Duncan",
-    date: new Date("11/15/2018"),
+    date: "11/15/2018",
     content:
       "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
   },
@@ -33,28 +33,62 @@ function listComments(commentArray) {
   commentsList.innerHTML = "";
 
   for (let i = 0; i < commentArray.length; i++) {
-    const commentItem = document.createElement("li");
+    const commentsItem = document.createElement("li");
+    commentsItem.classList.add("comments-list__item");
+
+    const commentsNameWrapper = document.createElement("div");
+    commentsNameWrapper.classList.add("comments-list__name-wrapper");
 
     const commentsNameVal = comments[i].name;
     const commentsNameElem = document.createElement("h2");
     commentsNameElem.innerText = commentsNameVal;
+    commentsItem.classList.add("comments-list__name");
+
+    const commentsDateWrapper = document.createElement("div");
+    commentsDateWrapper.classList.add("comments-list__date-wrapper");
 
     const commentsDateVal = comments[i].date;
     const commentsDateElem = document.createElement("h4");
     commentsDateElem.innerText = commentsDateVal;
-    commentsDateElem.innerText = comments[i].date.getMonth();
-    commentsDateElem.innerText += "-" + comments[i].date.getDay();
-    commentsDateElem.innerText += "-" + comments[i].date.getFullYear();
+    // commentsDateElem.innerText = comments[i].date.getMonth();
+    // commentsDateElem.innerText += "-" + comments[i].date.getDay();
+    // commentsDateElem.innerText += "-" + comments[i].date.getFullYear();
+    commentsItem.classList.add("comments-list__date");
+
+    const commentsContentWrapper = document.createElement("div");
+    commentsContentWrapper.classList.add("comments-list__content-wrapper");
 
     const commentsContentVal = comments[i].content;
     const commentsContentElem = document.createElement("h4");
     commentsContentElem.innerText = commentsContentVal;
+    commentsItem.classList.add("comments-list__content");
 
-    commentItem.appendChild(commentsNameElem);
-    commentItem.appendChild(commentsDateElem);
-    commentItem.appendChild(commentsContentElem);
+    const commentsImageWrapper = document.createElement("div");
+    commentsImageWrapper.classList.add("comments-list__image-wrapper");
 
-    commentsList.appendChild(commentItem);
+    const commentsImageVal = comments[i].image;
+    const commentsImageElem = document.createElement("img");
+    commentsImageElem.innerText = commentsImageVal;
+    commentsImageElem.src = ".././assets/icons/PNG/usericon-grey.png";
+    commentsImageElem.setAttribute("alt", "User Display Picture");
+    commentsItem.classList.add("comments-list__image");
+
+    //Adding the h2 to the wrapper div
+    commentsNameWrapper.appendChild(commentsNameElem);
+    commentsItem.appendChild(commentsNameWrapper);
+
+    //Adding the h4 to the wrapper div
+    commentsDateWrapper.appendChild(commentsDateElem);
+    commentsItem.appendChild(commentsDateWrapper);
+
+    //Adding the h4 to the wrapper div
+    commentsContentWrapper.appendChild(commentsContentElem);
+    commentsItem.appendChild(commentsContentWrapper);
+
+    commentsImageWrapper.appendChild(commentsImageElem);
+    commentsItem.appendChild(commentsImageWrapper);
+
+    commentsList.appendChild(commentsItem);
   }
 }
 
@@ -64,6 +98,7 @@ function displayComment(event) {
   event.preventDefault();
   const commentsNameVal = event.target.commentsName.value;
   const commentsContentVal = event.target.commentsContent.value;
+  //don't know if i like this below
   const commentsDateVal = new Date();
   if (commentsNameVal !== "" && commentsContentVal !== "") {
     comments.unshift({
@@ -71,9 +106,10 @@ function displayComment(event) {
       date: commentsDateVal,
       content: commentsContentVal,
     });
+    commentsForm.reset();
     listComments(comments);
   } else {
-    alert("please add your name and comment");
+    alert("please add your name and message");
   }
 }
 
--- sprint-two/scripts/shows.js
+++ sprint-two/scripts/shows.js
@@ -2,37 +2,37 @@ const shows = [
   {
     date: "Mon Dec 17 2018",
     venue: "Ronald Lane",
-    location: "San Fancisco, CA",
+    location: "San Francisco, CA",
     button: "BUY TICKETS",
   },
   {
     date: "Tue Jul 18 2019",
     venue: "Pier 3 East",
-    location: "San Fancisco, CA",
+    location: "San Francisco, CA",
     button: "BUY TICKETS",
   },
   {
     date: "Fri Jul 22 2019",
     venue: "View Lounge",
-    location: "San Fancisco, CA",
+    location: "San Francisco, CA",
     button: "BUY TICKETS",
   },
   {
     date: "Sat Aug 12 2019",
     venue: "Hyatt Agency",
-    location: "San Fancisco, CA",
+    location: "San Francisco, CA",
     button: "BUY TICKETS",
   },
   {
     date: "Fri Sep 05 2019",
     venue: "Moscow Center",
-    location: "San Fancisco, CA",
+    location: "San Francisco, CA",
     button: "BUY TICKETS",
   },
   {
     date: "Wed Aug 11 2019",
     venue: "Pres Club",
-    location: "San Fancisco, CA",
+    location: "San Francisco, CA",
     button: "BUY TICKETS",
   },
 ];
@@ -43,29 +43,54 @@ const container = document.querySelector(".container");
 
 function constructShows(shows) {
   for (show of shows) {
-    const showsConcert = document.createElement("div");
-    showsConcert.classList.add("showsConcert");
+    const showsConcert = document.createElement("li");
+    showsConcert.classList.add("concert-list__item");
+
+    const showsDateWrapper = document.createElement("div");
+    showsDateWrapper.classList.add("concert-list__date-wrapper");
 
     const showsConcertDate = document.createElement("h4");
-    showsConcertDate.innerHTML = show.showsConcertDate;
+    showsConcertDate.innerHTML = show.date;
+    showsConcertDate.classList.add("concert-list__date");
+
+    const showsVenueWrapper = document.createElement("div");
+    showsVenueWrapper.classList.add("concert-list__venue-wrapper");
 
     const showsConcertVenue = document.createElement("h4");
-    showsConcertVenue.innerHTML = show.showsConcertVenue;
+    showsConcertVenue.innerHTML = show.venue;
+    showsConcertVenue.classList.add("concert-list__venue");
+
+    const showsLocationWrapper = document.createElement("div");
+    showsLocationWrapper.classList.add("concert-list__location-wrapper");
 
     const showsConcertLocation = document.createElement("h4");
-    showsConcertLocation.innerHTML = show.showsConcertLocation;
+    showsConcertLocation.innerHTML = show.location;
+    showsConcertLocation.classList.add("concert-list__location");
+
+    const showsBtnWrapper = document.createElement("div");
+    showsBtnWrapper.classList.add("concert-list__btn-wrapper");
 
     const showsConcertBtn = document.createElement("button");
-    showsConcertBtn.innerHTML = show.showsConcertBtn;
+    showsConcertBtn.innerHTML = show.button;
+    showsConcertBtn.classList.add("concert-list__btn");
 
-    showsConcert.appendChild(date);
-    showsConcert.appendChild(venue);
-    showsConcert.appendChild(location);
-    showsConcert.appendChild(button);
+    //Adding the h4 to the wrapper div
+    showsDateWrapper.appendChild(showsConcertDate);
+    showsConcert.appendChild(showsDateWrapper);
+
+    //Adding the h4 to the wrapper div
+    showsVenueWrapper.appendChild(showsConcertVenue);
+    showsConcert.appendChild(showsVenueWrapper);
+
+    //Adding the h4 to the wrapper div
+    showsLocationWrapper.appendChild(showsConcertLocation);
+    showsConcert.appendChild(showsLocationWrapper);
+
+    //Adding the button to the wrapper div
+    showsBtnWrapper.appendChild(showsConcertBtn);
+    showsConcert.appendChild(showsBtnWrapper);
 
     container.appendChild(showsConcert);
-
-    // return container;
   }
 }
 
