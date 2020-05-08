2e2221fe1dabcbe90d4eb5192d58d608d3b070cb
Steven Martins <Steven@MacBook-Pro.local>
2020-05-07 23:17:19 -0700

    commiting my work from Thursday

--- sprint-three/scripts/axios.js
+++ sprint-three/scripts/axios.js
@@ -1,5 +1,3 @@
-// // Haven't linked any of this to my html yet!
-
 // // Comments
 // // Get
 // // https://project-1-api.herokuapp.com/comments?api_key=steven
@@ -145,66 +143,137 @@
 // // Get
 // // https://project-1-api.herokuapp.com/comments?api_key=steven
 
+// const createComments = (input) => {
+//   const commentsText = document.createElement("li");
+//   commentsText.textContent = input;
+//   commentsText.className = "comments-list__item";
+//   return commentsText;
+// };
+
+// const commentsList = document.querySelector(".comments__container");
+// const generateComments = (input) => {
+//   container.appendChild(createComments(input));
+
+//   const loadComments = () => {
+//     axios
+//       .get("https:project-1-api.herokuapp.com/comments?api_key=steven", {
+//         headers: {
+//           Accept: "text/plain",
+//         },
+//       })
+//       .then((response) => {
+//         const commentsArray = response.data;
+//         generateShow(`"${commentsArray}"`);
+//         // `"${index + 1} ${comments.name} ${comments.comment}"`);
+//       })
+//       .catch((err) => console.log(err));
+//   };
+
+//   loadComments();
+
+//   //do the same for PUT?
+
+// // // Shows
+// // // Get
+// // // https://project-1-api.herokuapp.com/showdates?api_key=steven
+
+// const createShow = (input) => {
+//   const showText = document.createElement("li");
+//   showText.textContent = input;
+//   showText.className = "concert-list__item";
+//   return showText;
+// };
+
+// const container = document.querySelector(".shows_container");
+// const generateShow = (input) => {
+//   container.appendChild(createShow(input));
+
+// const loadShows = () => {
+//   axios
+//     .get("https://project-1-api.herokuapp.com/showdates?api_key=steven", {
+//       headers: {
+//         Accept: "text/plain",
+//       },
+//     })
+//     .then((response) => {
+//       const showsArray = response.data;
+//       generateShow(`"${showsArray}"`);
+//       // input = response.data.all;
+//         // `"${index + 1} ${show.date} ${show.place} ${show.location}"`);
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+//   loadShows();
+
+//Ignore above for now
+
+// // Comments
+// // Get
+// // https://project-1-api.herokuapp.com/comments?api_key=steven
+
 const createComments = (input) => {
   const commentsText = document.createElement("li");
   commentsText.textContent = input;
   commentsText.className = "comments-list__item";
   return commentsText;
 };
-
 const commentsList = document.querySelector(".comments__container");
 const generateComments = (input) => {
   container.appendChild(createComments(input));
+};
+const loadComments = () => {
+  axios
+    .get("https:project-1-api.herokuapp.com/comments?api_key=steven")
+    .then((response) => {
+      const commentsArray = response.data;
+      generateShow(`"${commentsArray}"`);
+    })
+    .catch((err) => console.log(err));
+};
+loadComments();
 
-  const loadComments = () => {
-    axios
-      .get("https:project-1-api.herokuapp.com/comments?api_key=steven", {
-        headers: {
-          Accept: "text/plain",
-        },
-      })
-      .then((response) => {
-        const commentsArray = response.data;
-        generateShow(`"${commentsArray}"`);
-        // `"${index + 1} ${comments.name} ${comments.comment}"`);
-      })
-      .catch((err) => console.log(err));
-  };
+//do the same for PUT?
+// // Put
+// // https://project-1-api.herokuapp.com/comments?api_key=steven
 
-  loadComments();
-
-  //do the same for PUT?
+const loadMessage = () => {
+  axios
+    .post("https:project-1-api.herokuapp.com/comments?api_key=steven")
+    .then((response) => {
+      const newMessage = response.data;
+      generateMessage(`"${newMessage}"`);
+    })
+    .catch((err) => console.log(err));
+};
+loadComments();
 
 // // Shows
 // // Get
 // // https://project-1-api.herokuapp.com/showdates?api_key=steven
-
-
 const createShow = (input) => {
   const showText = document.createElement("li");
   showText.textContent = input;
   showText.className = "concert-list__item";
   return showText;
 };
-
 const container = document.querySelector(".shows_container");
 const generateShow = (input) => {
   container.appendChild(createShow(input));
-
+  for (let i = 0; i < input.length; i++) {
+    document.append(input[i]);
+    //  return input[i]; ???
+  }
+};
 const loadShows = () => {
   axios
-    .get("https://project-1-api.herokuapp.com/showdates?api_key=steven", {
-      headers: {
-        Accept: "text/plain",
-      },
-    })
+    .get("https://project-1-api.herokuapp.com/showdates?api_key=steven")
     .then((response) => {
       const showsArray = response.data;
-      generateShow(`"${showsArray}"`);
+      generateShow(`${showsArray}`);
       // input = response.data.all;
-        // `"${index + 1} ${show.date} ${show.place} ${show.location}"`);
+      // `"${index + 1} ${show.date} ${show.place} ${show.location}"`);
     })
     .catch((err) => console.log(err));
 };
-
-  loadShows();
+loadShows();
