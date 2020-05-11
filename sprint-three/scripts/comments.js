76d8a2695dca438d26c68de6d58394d357ee7dee
Steven Martins <Steven@MacBook-Pro.local>
2020-05-10 22:39:06 -0700

    final commit and final product for sprint three

--- sprint-three/scripts/comments.js
+++ sprint-three/scripts/comments.js
@@ -3,11 +3,9 @@
 const commentsList = document.getElementById("comments-list");
 const commentsForm = document.getElementById("comments-form");
 
-// Data Coming From API
+// Data Coming From API to create previous comments
 
 let commentArray = [];
-
-// let timestamp;
 
 const loadComments = () => {
   axios
@@ -31,29 +29,36 @@ function listComments(comments) {
 
   for (let i = 0; i < comments.length; i++) {
     console.log(comments[i]);
+    //creating li to append and adding class
     const commentsItem = document.createElement("li");
     commentsItem.classList.add("comments-list__item");
 
+    //creating div to append and adding class
     const commentsNameWrapper = document.createElement("div");
     commentsNameWrapper.classList.add("comments-list__name-wrapper");
 
+    //creating h2 to append and adding class
     const commentsNameVal = comments[i].name;
     const commentsNameElem = document.createElement("h2");
     commentsNameElem.innerText = commentsNameVal;
     commentsItem.classList.add("comments-list__name");
 
+    //creating div to append and adding class
     const commentsTimestampWrapper = document.createElement("div");
     commentsTimestampWrapper.classList.add("comments-list__date-wrapper");
 
+    //creating h4 to append and adding class
     const commentsTimestampVal = comments[i].timestamp;
     const commentsTimestampElem = document.createElement("h4");
     commentsTimestampElem.innerText = commentsTimestampVal;
     commentsTimestampElem.innerHTML = "05/10/2020";
     commentsItem.classList.add("comments-list__date");
 
+    //creating div to append and adding class
     const commentsImageWrapper = document.createElement("div");
     commentsImageWrapper.classList.add("comments-list__image-wrapper");
 
+    //creating img(profile picture) to append and adding class
     const commentsImageVal = comments[i].image;
     const commentsImageElem = document.createElement("img");
     commentsImageElem.innerText = commentsImageVal;
@@ -63,9 +68,11 @@ function listComments(comments) {
     commentsImageElem.setAttribute("alt", "User Display Picture");
     commentsItem.classList.add("comments-list__image");
 
+    //creating div to append and adding class
     const commentsCommentWrapper = document.createElement("div");
     commentsCommentWrapper.classList.add("comments-list__content-wrapper");
 
+    //creating h4 to append and adding class
     const commentsCommentVal = comments[i].comment;
     const commentsCommentElem = document.createElement("h4");
     commentsCommentElem.innerText = commentsCommentVal;
@@ -91,7 +98,7 @@ function listComments(comments) {
   }
 }
 
-// Event listeners
+// Event listener and generating new comment to be stored in backend
 
 commentsForm.addEventListener("submit", (event) => {
   event.preventDefault();
--- sprint-three/scripts/shows.js
+++ sprint-three/scripts/shows.js
@@ -19,33 +19,42 @@ const container = document.querySelector(".shows_container");
 
 function constructShows(shows) {
   for (show of shows) {
+    //creating li to append and adding class
     const showsConcert = document.createElement("li");
     showsConcert.classList.add("concert-list__item");
 
+    //creating div to append and adding class
     const showsDateWrapper = document.createElement("div");
     showsDateWrapper.classList.add("concert-list__date-wrapper");
 
+    //creating h4 to append and adding class
     const showsConcertDate = document.createElement("h4");
     showsConcertDate.innerHTML = show.date;
     showsConcertDate.classList.add("concert-list__date");
 
+    //creating div to append and adding class
     const showsPlaceWrapper = document.createElement("div");
     showsPlaceWrapper.classList.add("concert-list__venue-wrapper");
 
+    //creating h4 to append and adding class
     const showsConcertPlace = document.createElement("h4");
     showsConcertPlace.innerHTML = show.place;
     showsConcertPlace.classList.add("concert-list__venue");
 
+    //creating div to append and adding class
     const showsLocationWrapper = document.createElement("div");
     showsLocationWrapper.classList.add("concert-list__location-wrapper");
 
+    //creating h4 to append and adding class
     const showsConcertLocation = document.createElement("h4");
     showsConcertLocation.innerHTML = show.location;
     showsConcertLocation.classList.add("concert-list__location");
 
+    //creating div to append and adding class
     const showsBtnWrapper = document.createElement("div");
     showsBtnWrapper.classList.add("concert-list__btn-wrapper");
 
+    //creating button to append and adding class
     const showsConcertBtn = document.createElement("button");
     showsConcertBtn.innerHTML = "Buy Tickets";
     showsConcertBtn.classList.add("concert-list__btn");
